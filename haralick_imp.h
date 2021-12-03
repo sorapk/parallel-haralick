9515a2c73ac82766715656a71edb052cfdab27be
sorapk <sorapdev@gmail.com>
2021-12-03 01:06:31 -1000

    added sequential, sync_vertical_split , checks_gclm, img_data and gclm_data structs

--- haralick_imp.h
+++ haralick_imp.h
@@ -1,5 +1,23 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef HARALICK_IMP_H
+#define HARALICK_IMP_H
+
+#include "misc.h"
+
+/*
+    Structs
+*/
+typedef struct {
+    unsigned char* arr;
+    int width;
+    int height;
+} img_data;
+
+typedef struct {
+    int* arr;
+    int size;       //total array length  = size * size
+    int angle;
+    int dist;
+} gclm_data;
 
 
 /*
@@ -8,9 +26,12 @@
 unsigned char get_first_neighbor(unsigned char image[], int x, int y, int angle, int distance, int width, int height);
 unsigned char get_second_neighbor(unsigned char image[], int x, int y, int angle, int distance, int width, int height);
 int num_gray_levels(unsigned char image[], int size);
+void check_gclm(mpi_data mpi, img_data img, gclm_data target_gclm);
 
 /*
     Implementation
 */
+void sequential(mpi_data mpi, img_data img, gclm_data gclm);
+void sync_vertical_split(mpi_data mpi, img_data img, gclm_data gclm);
 
 #endif
